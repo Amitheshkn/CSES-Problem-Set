7ef1736d6a85c9f86a606b052a6e9c1d2a6de2f2
AMITHESH K N <46914484+Amitheshkn@users.noreply.github.com>
2021-10-20 20:47:19 +0530

    Update Movie_Festival.py

--- Sorting and Searching/Movie_Festival.py
+++ Sorting and Searching/Movie_Festival.py
@@ -1,13 +1,17 @@
-n=int(input())
-temp=[]
+n = int(input())
+intervals = []
+ 
 for i in range(n):
-    x,y  = map(int,input().split())
-    temp.append([y,x])
-temp.sort()
-ans=0
-end=0
-for i in range(n):
-    if temp[i][1]>=end:
-        ans+=1
-        end=temp[i][0]
+    a, b = map(int, input().split())
+    intervals.append([a, b])
+ 
+intervals.sort(key=lambda x: x[1])
+ 
+ans = 0
+end = -1
+for interval in intervals:
+    if interval[0] >= end:
+        ans += 1
+        end = interval[1]
 print(ans)
+ 
